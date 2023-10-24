7fd5e1a095f68f2163cfa8d476a1e7560f1d3cf7
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2023-10-24 17:51:55 +0200

    learning work

--- js/models/Movie.js
+++ js/models/Movie.js
@@ -1,3 +1,33 @@
 class Movie {
+    constructor(data){
+        this._duration = data.duration
+        this._picture = data.picture
+        this._released_in = data.released_in
+        this._synopsis = data.synopsis
+        this._title = data.title
+    }
 
+    get releasedIn(){
+        return this._released_in;
+    }
+
+    get picture(){
+        return `/assets/${this._picture}`;
+    }
+
+    get thumbnail(){
+        return `/assets/thumbnails/${this._picture}`;
+    }
+
+    get duration(){
+        return this._duration;
+    }
+
+    get synopsis(){
+        return this._synopsis;
+    }
+
+    get title(){
+        return this._title;
+    }
 }
